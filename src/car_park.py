57927394e7477744779797f505dbb43105f4c69a
Scott <winroof1988@gmail.com>
2023-11-29 02:26:22 +0800

    Added constructors and attributes to the car park class

--- src/car_park.py
+++ src/car_park.py
@@ -1,2 +1,9 @@
 class CarPark:
-    pass
+    def __init__(self, location, capacity, plates=None, sensors=None, displays=None):
+        self.location = location
+        self.capacity = capacity
+        self.plates = plates
+        self.sensors = sensors
+        self.displays = displays
+
+    def __str__(self):
